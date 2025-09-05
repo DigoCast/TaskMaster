7e52654a9aaa9ef0cd9a2daac6e63c0ff1fa642c
DigoCast <diegocastilholorenceto@gmail.com>
2025-09-05 10:45:26 -0300

    feat: add light theme

--- js/theme.js
+++ js/theme.js
@@ -1,22 +1,22 @@
 const themeToggle = document.getElementById("theme-toggle");
 const htmlElement = document.documentElement; 
+const lightIcon = document.getElementById('lightmode-ico');
+const darkIcon = document.getElementById('darkmode-ico');
 
 function setTheme(theme) {
   htmlElement.setAttribute("data-theme", theme);
-  localStorage.setItem("theme", theme);
-}
-
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme) {
-  setTheme(savedTheme);
-} else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-  setTheme("light");
-} else {
-  setTheme("dark");
 }
 
 themeToggle.addEventListener("click", () => {
   const currentTheme = htmlElement.getAttribute("data-theme");
   const newTheme = currentTheme === "dark" ? "light" : "dark";
   setTheme(newTheme);
+  if (newTheme === 'dark') {
+    lightIcon.style.display = 'block';
+    darkIcon.style.display = 'none';
+  } else {
+    lightIcon.style.display = 'none';
+    darkIcon.style.display = 'block';
+  }
 });
+
