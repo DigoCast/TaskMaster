ca6485e73ad9711e4beebdbad056ddd8a24a218a
DigoCast <diegocastilholorenceto@gmail.com>
2025-09-08 13:20:06 -0300

    feat: add function to mark as done a task

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 let tasks = [];
 let defaultMessage = document.getElementById("default-message");
+let defaultMessageConcluded = document.getElementById("default-message-concluded");
 let container = document.getElementById("container-tasks");
+let listConcluded = document.getElementById("list-concluded-tasks");
+let containerConcluded = document.getElementById("list-concluded-tasks");
 
 function updateDefaultMessage() {
-  if (container.children.length === 0) {
-    defaultMessage.style.display = "flex";
-  } else {
-    defaultMessage.style.display = "none";
-  }
+    defaultMessage.style.display = container.children.length === 0 ? "flex" : "none";
+    defaultMessageConcluded.style.display = listConcluded.children.length === 0 ? "flex" : "none" ; 
 }
 
 function addTask() {
@@ -51,17 +51,28 @@ function addTask() {
               </div>
             `;
 
-  const taskObject = {taskName, taskPriority, taskPriority, taskType, taskDescription}
-  tasks.push(taskObject);
-  console.log(tasks)
-  
-  
   const deleteButton = newTask.querySelector(".delete-button");
   deleteButton.addEventListener("click", function(){
     newTask.remove();
     updateDefaultMessage();
   })
 
+  const concludedButton = newTask.querySelector(".concluded-button");
+  concludedButton.addEventListener("click", function(){
+    newTask.classList.remove("card-task");
+    newTask.classList.add("card-concluded-task");
+    updateDefaultMessage();
+  })
+
+  containerConcluded.appendChild(newTask)
+
+
+  const taskObject = {taskName, taskPriority, taskPriority, taskType, taskDescription}
+  tasks.push(taskObject);
+  console.log(tasks)
+  
+  
+  
   container.appendChild(newTask);
   updateDefaultMessage();
 
